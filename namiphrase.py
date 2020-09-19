abe08f555758b776e4fad29a27e5015561181da9
Hasebe Masahiko <JCA03205@gmail.com>
2020-09-20 08:13:56 +0900

    duration,velocityにも繰り返し機能実装

--- namiphrase.py
+++ namiphrase.py
@@ -51,9 +51,10 @@ class PhraseGenerator():
                             repeatCount = 1
                         else:
                             if nt[num+1:].isdecimal() == True: repeatCount = int(nt[num+1:])
+                        rptEndPtr = i+1
                         for j in range(repeatCount):
-                            insPtr = i+1+j*(i+1-repeatStart)
-                            noteFlow[insPtr:insPtr] = noteFlow[repeatStart:i+1]
+                            insPtr = rptEndPtr + j*(rptEndPtr-repeatStart)
+                            noteFlow[insPtr:insPtr] = noteFlow[repeatStart:rptEndPtr]
                         break
             ## end of for
 
@@ -74,9 +75,10 @@ class PhraseGenerator():
                     if nt[reCnt+1:reCnt+2] == '*' and firstBracket == True:
                         if nt[reCnt+2:].isdecimal() == True: repeatCount = int(nt[reCnt+2:])
                         if repeatCount > 1:
+                            rptEndPtr = i+1
                             for j in range(repeatCount-1):
-                                insPtr = i+1+j*(i+1-repeatStart)
-                                noteFlow[insPtr:insPtr] = noteFlow[repeatStart:i+1]
+                                insPtr = rptEndPtr + j*(rptEndPtr-repeatStart)
+                                noteFlow[insPtr:insPtr] = noteFlow[repeatStart:rptEndPtr]
                     break
             ## end of for
         ## end of while
@@ -101,46 +103,126 @@ class PhraseGenerator():
 
         return noteFlow, len(noteFlow)
 
-    def fillOmittedData(self):
-        ### Note
-        noteFlow, ntNum = self.fillOmittedNoteData()
-
-        ### Duration
+    def fillOmittedDurData(self, durText, ntNum):
         durFlow = []
-        if ',' in self.noteData[1]:
-            durFlow = re.split('[,|]', self.noteData[1].replace(' ',''))
+        if ',' in durText:
+            durFlow = re.split('[,|]', durText.replace(' ',''))
         else:
             # ','が無い場合、全体を一つの文字列にし、リストとして追加
-            durFlow.append(self.noteData[1])
+            durFlow.append(durText)
 
-        # セミコロンの後で設定されている基本音価の調査
+        noRepeat = False
+        while noRepeat == False:
+            noRepeat = True
+            repeatStart = 0
+            firstBracket = False
+            for i, dur in enumerate(durFlow):   # <  >*n
+                if '<' in dur:
+                    noRepeat = False
+                    locate = dur.find('<')
+                    if locate == 0:
+                        durFlow[i] = dur[1:]
+                        repeatStart = i
+                        firstBracket = True
+                elif '>' in dur:
+                    reCnt = dur.rfind('>')
+                    durFlow[i] = dur[0:reCnt]
+                    if dur[reCnt+1:reCnt+2] == '*' and firstBracket == True:
+                        repeatCount = 0
+                        if dur[reCnt+2:].isdecimal() == True: repeatCount = int(dur[reCnt+2:])
+                        if repeatCount > 1:
+                            rptEndPtr = i+1
+                            for j in range(repeatCount-1):
+                                insPtr = rptEndPtr + j*(rptEndPtr-repeatStart)
+                                durFlow[insPtr:insPtr] = durFlow[repeatStart:rptEndPtr]
+                    elif i+1 == len(durFlow) and firstBracket == True:
+                        cntr = 0
+                        while True:
+                            durFlow.append(durFlow[repeatStart+cntr])
+                            cntr+=1
+                            if ntNum <= len(durFlow): break
+                    break
+            ## end of for
+        ## end of while
+
         durNum = len(durFlow)
-        if ':' in durFlow[durNum-1]:
-            lastDurTxt = durFlow[durNum-1].replace(' ','').split(':')
-            if lastDurTxt[0] == '':
-                lastDurTxt[0] = '1'
-            durFlow[durNum-1] = lastDurTxt[0]
-            if lastDurTxt[1].isdecimal() == True:
-                self.onpu = int(lastDurTxt[1])
-            else:
-                self.onpu = 4
-
         if durNum < ntNum:
             for _ in range(ntNum-durNum):
                 durFlow.append(durFlow[durNum-1])   # 足りない要素を補填
+        elif durNum > ntNum:
+            del durFlow[ntNum:] # 多い要素を削除
+        return durFlow
 
-        ### Velocity
+    def fillOmittedVelData(self, velText, ntNum):
         velFlow = []
-        if ',' in self.noteData[2]:
-            velFlow = re.split('[,|]', self.noteData[2].replace(' ',''))
+        if ',' in velText:
+            velFlow = re.split('[,|]', velText.replace(' ',''))
         else:
             # ','が無い場合、全体を一つの文字列にし、リストとして追加
-            velFlow.append(self.noteData[2])
+            velFlow.append(velText)
+
+        noRepeat = False
+        while noRepeat == False:
+            noRepeat = True
+            repeatStart = 0
+            firstBracket = False
+            for i, vel in enumerate(velFlow):   # <  >*n
+                if '<' in vel:
+                    noRepeat = False
+                    locate = vel.find('<')
+                    if locate == 0:
+                        velFlow[i] = vel[1:]
+                        repeatStart = i
+                        firstBracket = True
+                elif '>' in vel:
+                    reCnt = vel.rfind('>')
+                    velFlow[i] = vel[0:reCnt]
+                    if vel[reCnt+1:reCnt+2] == '*' and firstBracket == True:
+                        repeatCount = 0
+                        if vel[reCnt+2:].isdecimal() == True: repeatCount = int(vel[reCnt+2:])
+                        if repeatCount > 1:
+                            rptEndPtr = i+1
+                            for j in range(repeatCount-1):
+                                insPtr = rptEndPtr + j*(rptEndPtr-repeatStart)
+                                velFlow[insPtr:insPtr] = velFlow[repeatStart:rptEndPtr]
+                    elif i+1 == len(velFlow) and firstBracket == True:
+                        cntr = 0
+                        while True:
+                            velFlow.append(velFlow[repeatStart+cntr])
+                            cntr+=1
+                            if ntNum <= len(velFlow): break
+                    break
+            ## end of for
+        ## end of while
 
         velNum = len(velFlow)
         if velNum < ntNum:
             for _ in range(ntNum-velNum):
                 velFlow.append(velFlow[velNum-1])   # 足りない要素を補填
+        return velFlow
+
+    def fillOmittedData(self):
+        ### Note
+        noteFlow, ntNum = self.fillOmittedNoteData()
+
+        ### Duration
+        # セミコロンの後で設定されている基本音価の調査
+        durText = self.noteData[1]
+        if ':' in durText:
+            lastDurTxt = durText.split(':')
+            if lastDurTxt[0] == '':
+                durText = '1'
+            else:
+                durText = lastDurTxt[0]
+            if lastDurTxt[1].isdecimal() == True:
+                self.onpu = int(lastDurTxt[1])
+            else:
+                self.onpu = 4
+        durFlow = self.fillOmittedDurData(durText, ntNum)
+
+        ### Velocity
+        velFlow = self.fillOmittedVelData(self.noteData[2], ntNum)
+
         return noteFlow, durFlow, velFlow, ntNum
 
     def cnvNoteToPitch(self, noteText):
