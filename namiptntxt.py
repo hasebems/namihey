dc3af144488a546ab1a1ffb447c0b394e26cd1d3
HASEBE Masahiko <JCA03205@gmail.com>
2022-02-26 20:32:34 +0900

    PtnTxtクラスの追加

---
+++ namiptntxt.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+class PtnTxt:
+    def complement_bracket(self, input_text):
+        # [] のセットを抜き出し、中身を note_info に入れる
+        note_info = []
+        tx = input_text
+        while True:
+            num = tx.find(']')
+            if num == -1:
+                break
+            note_info.append(tx[1:num])
+            tx = tx[num + 1:].strip()
+            if len(tx) == 0:
+                break
+            if tx[0:1] != '[':
+                break
+
+        # [] の数が 1,2 の時は中身を補填
+        bracket_num = len(note_info)
+        if bracket_num == 1:
+            note_info.append('1')  # set default value
+            note_info.append('100')
+        elif bracket_num == 2:
+            note_info.append('100')  # set default velocity value
+        elif bracket_num == 0 or bracket_num > 3:
+            # [] の数が 1〜3 以外ならエラー
+            return None
+
+        note_info.insert(0, 'phrase')
+        return note_info
+
+    def complement_brace(self, input_text):
+        # {} のセットを抜き出し、中身を note_info に入れる
+        note_info = []
+        dialogue = ''
+        tx = input_text
+        while True:
+            num = tx.find('}')
+            if num == -1:
+                break
+            note_info.append(tx[1:num])
+            tx = tx[num + 1:].strip()
+            if len(tx) == 0:
+                break
+            if tx[0:1] != '{':
+                break
+
+        # [] の数が 1,2 の時は中身を補填
+        brktNum = len(note_info)
+        if brktNum == 1:
+            note_info.append('1')  # set default value
+            note_info.append('100')
+        elif brktNum == 2:
+            note_info.append('100')  # set default velocity value
+        elif brktNum == 0 or brktNum > 3:
+            # [] の数が 1〜3 以外ならエラー
+            return None
+
+        if note_info[0][0:3] == 'rnd':
+            note_info.insert(0, 'random')
+            dialogue = "set Random Pattern!"
+        elif note_info[0][0:3] == 'arp':
+            note_info.insert(0, 'arp')
+            dialogue = "set Arpeggio Pattern!"
+        else:
+            return None
+        return note_info, dialogue
+
+    def set_txt_to_block(self, blk, txt):
+        # complement_bracket/brace で note_info[] を得た後に txt に入れて呼ばれる
+        blk.clear_description()
+        blk.add_seq_description(txt)
+
